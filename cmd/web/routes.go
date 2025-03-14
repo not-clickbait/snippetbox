7ba691e4392f0fc54555980205e5ce43c8d6d1d4
Gönczöl Armand <gonczol.armand@gmail.com>
2025-03-14 16:59:46 +0100

    feat: Use sessions to display toast/flash messages

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,14 +8,17 @@ import (
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
 	// fileServer := http.FileServer(http.Dir("./ui/static/"))
 	fileServer := http.FileServer(&nfs.NeuteredFileSystem{Fs: http.Dir("./ui/static/")})
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", app.home) // terminate / with ${$} so it won't act as a catch-all
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home)) // terminate / with ${$} so it won't act as a catch-all
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	// return mux
 
--- init/init.sql
+++ init/init.sql
@@ -6,6 +6,13 @@ CREATE TABLE snippets (
     expires DATETIME
 );
 
+CREATE TABLE sessions (
+    token CHAR(43) PRIMARY KEY,
+    data BLOB NOT NULL,
+    expiry TIMESTAMP(6) NOT NULL
+);
+CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
 CREATE USER 'web'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE ON snippetbox.* TO 'web'@'%';
 ALTER USER 'web'@'%' IDENTIFIED BY 'web';
